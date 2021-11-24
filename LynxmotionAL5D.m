9d4c880035d8f2a76fbb66558f41940ee56f8c4a
joejeffcock <sc18j3j@leeds.ac.uk>
2021-11-24 20:53:25 +0000

    feat: draw function + added joint 5

--- LynxmotionAL5D.m
+++ LynxmotionAL5D.m
@@ -1,24 +1,29 @@
 classdef LynxmotionAL5D
     properties (Constant)
+        % link lengths
         d1 = 5
         d2 = 10
         d3 = 10
         d4 = 5
+        % gripper finger lengths
+        f1 = 2
+        f2 = 2
     end
     properties
         eef_pose
     end
     methods
-        function T = forward_kinematics(obj, q1, q2, q3, q4)
+        function T = forward_kinematics(obj, q1, q2, q3, q4, q5)
             % transforms of lynxmotion
             T01  = tf_from_distal(0, deg2rad(90), obj.d1, q1);
             T12  = tf_from_distal(obj.d2, 0, 0, q2);
             T23  = tf_from_distal(obj.d3, 0, 0, q3);
             T33p = tf_from_distal(0, deg2rad(90), 0, q4);
             T3p4 = tf_from_distal(0, 0, obj.d4, 0);
+            T45  = tf_from_distal(0, 0, 0, q5);
 
             % forward kinematics
-            T = T01 * T12 * T23 * T33p * T3p4;
+            T = T01 * T12 * T23 * T33p * T3p4 * T45;
         end
 
         function joints = inverse_kinematics(obj, x, y, z, psi, mu)
@@ -52,6 +57,42 @@ classdef LynxmotionAL5D
             joints = [q1 q2 q3 q4 q5];
         end
 
+        function draw(obj, q1, q2, q3, q4, q5)
+            % transforms of lynxmotion
+            T = zeros(6,4,4);
+            T(1,:,:) = eye(4);
+            T(2,:,:) = [tf_from_distal(0, deg2rad(90), obj.d1, q1)];
+            T(3,:,:) = [tf_from_distal(obj.d2, 0, 0, q2)];
+            T(4,:,:) = [tf_from_distal(obj.d3, 0, 0, q3)];
+            T(5,:,:) = [tf_from_distal(0, deg2rad(90), 0, q4)];
+            T(6,:,:) = [tf_from_distal(0, 0, obj.d4, 0)];
+            T(7,:,:) = [tf_from_distal(0, 0, 0, q5)];
+
+            % joint positions from FK
+            T0eef = eye(4);
+            points = zeros(7,3);
+            for i = 1:7
+                T0eef = T0eef * squeeze(T(i,:,:));
+                points(i,1) = T0eef(1,4);
+                points(i,2) = T0eef(2,4);
+                points(i,3) = T0eef(3,4);
+            end
+
+            % gripper points
+            T0g1f1 = T0eef  * [tf_from_distal( obj.f1, 0, 0     , 0)];
+            T0g1f2 = T0g1f1 * [tf_from_distal( 0     , 0, obj.f2, 0)];
+            T0g2f1 = T0eef  * [tf_from_distal(-obj.f1, 0, 0     , 0)];
+            T0g2f2 = T0g2f1 * [tf_from_distal( 0     , 0, obj.f2, 0)];
+            g1 = [T0eef(1:3, 4) T0g1f1(1:3, 4) T0g1f2(1:3, 4)];
+            g2 = [T0eef(1:3, 4) T0g2f1(1:3, 4) T0g2f2(1:3, 4)];
+
+            % plot points
+            plot3(points(:,1), points(:,2), points(:,3), 'ko-', 'Linewidth', 2);
+            hold on
+            plot3(g1(1,:), g1(2,:), g1(3,:), 'k-', 'Linewidth', 2);
+            hold on
+            plot3(g2(1,:), g2(2,:), g2(3,:), 'k-', 'Linewidth', 2);
+        end
     end
 end
 
